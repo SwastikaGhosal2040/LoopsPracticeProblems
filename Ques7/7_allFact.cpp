a593c03d9e816fc3d89461f54695e5c256622093
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-10 03:09:14 +0530

    Add files via upload

---
+++ Ques7/7_allFact.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int fact=1;
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+      fact=fact*i;
+      cout<<i<<" ! = "<<fact<<endl;
+    }
+    
+}
