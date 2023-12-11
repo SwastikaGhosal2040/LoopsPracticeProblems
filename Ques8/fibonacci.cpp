4f9beaab44fecacdf324f82655cf602aa5310987
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-11 19:24:23 +0530

    Add files via upload

---
+++ Ques8/fibonacci.cpp
@@ -0,0 +1,19 @@
+//print the nth fibonacci number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int a = 1;
+    int b = 1;
+    int sum = 0;
+    for(int i=1;i<=n-2;i++)
+    {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+    cout<<n<<"th fibonacci number : "<<b;
+}
