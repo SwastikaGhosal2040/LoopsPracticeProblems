1293d19c859f5d4a214e83831377d6becf29ce74
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-16 15:29:09 +0530

    Add files via upload

---
+++ Pattern-printing/Ques1/1a_SolidRectangle.cpp
@@ -0,0 +1,25 @@
+/*
+Print the solid rectangle
+*****
+*****
+*****
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int m;
+    cout<<"Enter number of columns : ";
+    cin>>m;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
