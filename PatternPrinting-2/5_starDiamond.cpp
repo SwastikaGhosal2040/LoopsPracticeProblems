5057cf9b00080d968a2e7bdd17a3069e0a01938f
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-06 10:56:55 +0530

    Add files via upload

---
+++ PatternPrinting-2/5_starDiamond.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter row no: ";
+    cin>>n;
+    int nst=1;
+    int nsp=n-1;
+    for(int i=1;i<=2*n-1;i++)
+    {
+        //spaces
+        for(int j=1;j<=nsp;j++)
+        {
+            cout<<" "<<"  ";
+        }
+        if(i<=n-1) nsp--;
+        else       nsp++;
+
+        //stars
+        for(int k=1;k<=nst;k++)
+        {
+            cout<<"*"<<"  ";
+        }
+        if(i<=n-1) nst+=2;
+        else       nst-=2;
+        
+        cout<<endl;
+    }
+}
